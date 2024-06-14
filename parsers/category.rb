01f443a90596b76cb1cec7a2995c85fd4d35a037
hraihanm <raihanmuhamad@gmail.com>
2024-06-15 02:25:13 +0700

    changed header

--- parsers/category.rb
+++ parsers/category.rb
@@ -28,14 +28,14 @@ main_categories.each do |main_cat|
       page_type: 'listings',
       headers: {
           'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0',
-          # 'Accept-Encoding' => 'gzip, deflate, br',
-          # 'Accept-Language' => 'en-US,en;q=0.9',
-          # 'Cache-Control' => 'max-age=0',
-          # 'Accept' => 'application/json, text/javascript, */*; q=0.01',
-          # 'Origin' => 'https://naivas.online/',
-          # 'Referer' => 'https://naivas.online/',
-          # 'X-Requested-With' => 'XMLHttpRequest',
-          # 'Upgrade-Insecure-Requests' => 1
+          'Accept-Encoding' => 'gzip, deflate, br',
+          'Accept-Language' => 'en-US,en;q=0.9',
+          'Cache-Control' => 'max-age=0',
+          'Accept' => 'application/json, text/javascript, */*; q=0.01',
+          'Origin' => 'https://naivas.online/',
+          'Referer' => 'https://naivas.online/',
+          'X-Requested-With' => 'XMLHttpRequest',
+          'Upgrade-Insecure-Requests' => 1
       },
       vars: {
           category_name: cat_name,
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -18,20 +18,20 @@ pages << {
     #     },
     # }
     headers: {
-        # 'Host' => 'naivas.online',
+        'Host' => 'naivas.online',
         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0',
-        # 'Accept-Encoding' => 'gzip, deflate, br, zstd',
-        # 'Accept-Language' => 'en-US,en;q=0.9',
-        # 'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        # 'Sec-Fetch-Dest' => 'document',
-        # 'Sec-Fetch-Mode' => 'navigate',
-        # 'Sec-Fetch-Site' => 'cross-site',
-        # 'Referer' => 'https://www.google.com/',
-        # 'Connection' => 'keep-alive',
-        # 'Upgrade-Insecure-Requests' => 1,
-        # 'Sec-Ch-Ua-Platform' => '"Windows"',
-        # 'Sec-Ch-Ua-Mobile' => '?0',
-        # 'Sec-Ch-Ua' => '"Chromium";v="124", "Opera";v="110", "Not-A.Brand";v="99"'
+        'Accept-Encoding' => 'gzip, deflate, br, zstd',
+        'Accept-Language' => 'en-US,en;q=0.9',
+        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Sec-Fetch-Dest' => 'document',
+        'Sec-Fetch-Mode' => 'navigate',
+        'Sec-Fetch-Site' => 'cross-site',
+        'Referer' => 'https://www.google.com/',
+        'Connection' => 'keep-alive',
+        'Upgrade-Insecure-Requests' => 1,
+        'Sec-Ch-Ua-Platform' => '"Windows"',
+        'Sec-Ch-Ua-Mobile' => '?0',
+        'Sec-Ch-Ua' => '"Chromium";v="124", "Opera";v="110", "Not-A.Brand";v="99"'
     }
     # headers: {
     #     # 'Host' => 'naivas.online',
