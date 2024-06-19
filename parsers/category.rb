e3d4ba5fed2c41e38e9707ac6573494b809365b5
Hammam Raihan <raihanmuhamad@gmail.com>
2024-06-19 19:16:13 +0700

    header change

--- parsers/category.rb
+++ parsers/category.rb
@@ -1,22 +1,20 @@
 html = Nokogiri::HTML(content)
 
 headers = {
-  'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  'Accept-Encoding' => 'gzip, deflate, br, zstd',
-  'Accept-Language' => 'en-US,en;q=0.9',
-  # 'Connection' => 'keep-alive',
-  'Cookie' => '_ga_MRWW8LRT1C=GS1.1.1718515153.1.0.1718515153.0.0.0; _ga=GA1.1.822121844.1718515153; axeptio_cookies={%22$$token%22:%22t1v1w821zpyn4r5rvtwdd%22%2C%22$$date%22:%222024-06-16T05:19:22.179Z%22%2C%22$$cookiesVersion%22:{}%2C%22$$completed%22:false}; axeptio_authorized_vendors=%2C%2C; axeptio_all_vendors=%2C%2C; XSRF-TOKEN=eyJpdiI6IlJESllNNWErNlN0RTMwQ29qTTBMSHc9PSIsInZhbHVlIjoiYVVCWVZhZGp3UXk4NTBJcjVheXh5QXRnV2tEa3VDeEZ0OVBzRzBqcFhsbHpscmN1amxEMzdLVXR1TWphVWlaRGVsMnNWTXV0bmpkV0p3MGxrN2N3QkZHZWxrZlY4UmQzMDBZRkZycCtXRW16NVhnWWlFVkFtZURRSk5YK3h4bzgiLCJtYWMiOiJjYTAyNDViOTBkODUzMGUxYjE3MzA0M2JlYjkwNGU5ZmRjM2UzMmQ0ZmIwOTM3MWM5NzQwOTg5OGNjN2UxZGQyIiwidGFnIjoiIn0%3D; naivas_session=eyJpdiI6ImFnVTFYb3FtUkJlUTlXOVZ3RGtGRHc9PSIsInZhbHVlIjoibjVVNXAyNG9ISGtRbHBwd05ob3o4ZHFJVkx6T2pDMm4ySlI0S3NBcFhDa0hieWZMZFphS0ZTZzhPU3pwTWlsUVBwUkljVU5WV2x1MENkdlUvNmEwYWwzZ0pVS1Zub01yVHdJZmRHeFk3RWhEK3FlR09ZTmxYZ3dyS0E2Q1d4TEMiLCJtYWMiOiIzZmY1MjMxMjQ3YjM3YjU4YWVlZTRkYTNiOTk0ZTJkZTIwYjY3NmEyMTM4ZTBjNjM0MjlmNThkNDZmMzdhN2E0IiwidGFnIjoiIn0%3D',
-  # 'Host' => 'naivas.online',
-  'Referer' => page['url'],
-  'Sec-Fetch-Dest' => 'document',
-  'Sec-Fetch-Mode' => 'navigate',
-  'Sec-Fetch-Site' => 'same-origin',
-  'Sec-Fetch-User' => '?1',
-  'Upgrade-Insecure-Requests' => '1',
-  'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0',
-  'sec-ch-ua' => '"Chromium";v="124", "Opera";v="110", "Not-A.Brand";v="99"',
-  'sec-ch-ua-mobile' => '?0',
-  'sec-ch-ua-platform' => '"Windows"'
+    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding' => 'gzip, deflate, br, zstd',
+    'Accept-Language' => 'en-US,en;q=0.9',
+    'Cookie' => 'axeptio_authorized_vendors=%2Csnapchat%2Cfacebook_conversion_tracking%2Cfacebook_pixel%2Ctwitter%2Cgmaps%2Cgoogle_ads%2Cgoogle_analytics%2Cgoogle_conversion_tracking%2Cide%2Cgoogletagmanager%2Chotjar%2CSnapchat%2CFacebook%20Conversion%20Tracking%2CTwitter%2CGoogle_Ads%2CGoogle_Conversion_Tracking%2CIDE%2C; axeptio_all_vendors=%2Csnapchat%2Cfacebook_conversion_tracking%2Cfacebook_pixel%2Ctwitter%2Cgmaps%2Cgoogle_ads%2Cgoogle_analytics%2Cgoogle_conversion_tracking%2Cide%2Cgoogletagmanager%2Chotjar%2CSnapchat%2CFacebook%20Conversion%20Tracking%2CTwitter%2CGoogle_Ads%2CGoogle_Conversion_Tracking%2CIDE%2C; axeptio_cookies={%22$$token%22:%22woalm5lltpms4hppajaetf%22%2C%22$$date%22:%222024-06-19T11:06:40.251Z%22%2C%22$$cookiesVersion%22:{%22name%22:%22naivas-en%22%2C%22identifier%22:%22645cb096c665b447c8563f27%22}%2C%22snapchat%22:true%2C%22facebook_conversion_tracking%22:true%2C%22facebook_pixel%22:true%2C%22twitter%22:true%2C%22gmaps%22:true%2C%22google_ads%22:true%2C%22google_analytics%22:true%2C%22google_conversion_tracking%22:true%2C%22ide%22:true%2C%22googletagmanager%22:true%2C%22hotjar%22:true%2C%22Snapchat%22:true%2C%22Facebook%20Conversion%20Tracking%22:true%2C%22Twitter%22:true%2C%22Google_Ads%22:true%2C%22Google_Conversion_Tracking%22:true%2C%22IDE%22:true%2C%22$$completed%22:true}; XSRF-TOKEN=eyJpdiI6Ik1CSWVtdFNnR1Bac1RwT1pTbk15WVE9PSIsInZhbHVlIjoiSkEzblF1SDZWc2d0TVpFdWxyeE5ZWXdnN2xRWWVybE8xR1A1MDkzNnJFSlozUC84bFE0STRaN1ViZTJrZjdSVXVFSG54OUdaRmVyU1BtRDNneGdCalB0NWE2TE9VbDhaWHU0UUhHS3FjZEhLRmtDV2FvUVkxa0hQN1ZJU1hoV2QiLCJtYWMiOiI1ZGQ3ODI4N2ZiNmViYTFmMjk2ODJjNTBlOWM1NDI4ZGJkOWU2NzJhMGEzMjQyNWY3NmRlZTM5NTBlOTM1ZDljIiwidGFnIjoiIn0%3D; naivas_session=eyJpdiI6ImZjUzhzcGVCOGw4bVpQdEpWRm9FSXc9PSIsInZhbHVlIjoiOUVIYVB2aGJ2V21haFQxR3VPTklESjhjSDFoSzdjbkdsL3RDM1BjRlFaWDk4emZ4Q2hqd3VGSE9EQ1hieVJLR3Izem11N2hJNHh6M1Q0N1VvL2dadlIxYmdlU0JwNEs4YkE4SjQ4SWdOUmlwSklZNmdIRjVkK3ZNZ2NtVE1kMzQiLCJtYWMiOiIwNDgxMjkyMjkyZmNmNmY4YWYxZjcyNzRmNDRhN2YzYzMwZWYzOGVhM2QwMzcxMzk0ZDEwNTYyZDA4MThjZDE1IiwidGFnIjoiIn0%3D',
+    'Priority' => 'u=0, i',
+    'Referer' => 'https://www.google.com/',
+    'Sec-Ch-Ua' => '"Opera";v="111", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'Sec-Ch-Ua-Mobile' => '?0',
+    'Sec-Ch-Ua-Platform' => '"Windows"',
+    'Sec-Fetch-Dest' => 'document',
+    'Sec-Fetch-Mode' => 'navigate',
+    'Sec-Fetch-Site' => 'cross-site',
+    'Upgrade-Insecure-Requests' => '1',
+    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0'
 }
 
 # 1st Level
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -18,17 +18,19 @@ pages << {
     #     },
     # }
     headers: {
-        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding' => 'gzip, deflate, br, zstd',
-        'Accept-Language' => 'en-US,en;q=0.9',
-        'Referer' => 'https://www.google.com/',
-        'Sec-Fetch-Dest' => 'document',
-        'Sec-Fetch-Mode' => 'navigate',
-        'Sec-Fetch-Site' => 'cross-site',
-        'Upgrade-Insecure-Requests' => '1',
-        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0',
-        'sec-ch-ua' => '"Chromium";v="124", "Opera";v="110", "Not-A.Brand";v="99"',
-        'sec-ch-ua-mobile' => '?0',
-        'sec-ch-ua-platform' => '"Windows"'
-      }
+      'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'Accept-Encoding' => 'gzip, deflate, br, zstd',
+      'Accept-Language' => 'en-US,en;q=0.9',
+      'Cookie' => 'axeptio_authorized_vendors=%2Csnapchat%2Cfacebook_conversion_tracking%2Cfacebook_pixel%2Ctwitter%2Cgmaps%2Cgoogle_ads%2Cgoogle_analytics%2Cgoogle_conversion_tracking%2Cide%2Cgoogletagmanager%2Chotjar%2CSnapchat%2CFacebook%20Conversion%20Tracking%2CTwitter%2CGoogle_Ads%2CGoogle_Conversion_Tracking%2CIDE%2C; axeptio_all_vendors=%2Csnapchat%2Cfacebook_conversion_tracking%2Cfacebook_pixel%2Ctwitter%2Cgmaps%2Cgoogle_ads%2Cgoogle_analytics%2Cgoogle_conversion_tracking%2Cide%2Cgoogletagmanager%2Chotjar%2CSnapchat%2CFacebook%20Conversion%20Tracking%2CTwitter%2CGoogle_Ads%2CGoogle_Conversion_Tracking%2CIDE%2C; axeptio_cookies={%22$$token%22:%22woalm5lltpms4hppajaetf%22%2C%22$$date%22:%222024-06-19T11:06:40.251Z%22%2C%22$$cookiesVersion%22:{%22name%22:%22naivas-en%22%2C%22identifier%22:%22645cb096c665b447c8563f27%22}%2C%22snapchat%22:true%2C%22facebook_conversion_tracking%22:true%2C%22facebook_pixel%22:true%2C%22twitter%22:true%2C%22gmaps%22:true%2C%22google_ads%22:true%2C%22google_analytics%22:true%2C%22google_conversion_tracking%22:true%2C%22ide%22:true%2C%22googletagmanager%22:true%2C%22hotjar%22:true%2C%22Snapchat%22:true%2C%22Facebook%20Conversion%20Tracking%22:true%2C%22Twitter%22:true%2C%22Google_Ads%22:true%2C%22Google_Conversion_Tracking%22:true%2C%22IDE%22:true%2C%22$$completed%22:true}; XSRF-TOKEN=eyJpdiI6Ik1CSWVtdFNnR1Bac1RwT1pTbk15WVE9PSIsInZhbHVlIjoiSkEzblF1SDZWc2d0TVpFdWxyeE5ZWXdnN2xRWWVybE8xR1A1MDkzNnJFSlozUC84bFE0STRaN1ViZTJrZjdSVXVFSG54OUdaRmVyU1BtRDNneGdCalB0NWE2TE9VbDhaWHU0UUhHS3FjZEhLRmtDV2FvUVkxa0hQN1ZJU1hoV2QiLCJtYWMiOiI1ZGQ3ODI4N2ZiNmViYTFmMjk2ODJjNTBlOWM1NDI4ZGJkOWU2NzJhMGEzMjQyNWY3NmRlZTM5NTBlOTM1ZDljIiwidGFnIjoiIn0%3D; naivas_session=eyJpdiI6ImZjUzhzcGVCOGw4bVpQdEpWRm9FSXc9PSIsInZhbHVlIjoiOUVIYVB2aGJ2V21haFQxR3VPTklESjhjSDFoSzdjbkdsL3RDM1BjRlFaWDk4emZ4Q2hqd3VGSE9EQ1hieVJLR3Izem11N2hJNHh6M1Q0N1VvL2dadlIxYmdlU0JwNEs4YkE4SjQ4SWdOUmlwSklZNmdIRjVkK3ZNZ2NtVE1kMzQiLCJtYWMiOiIwNDgxMjkyMjkyZmNmNmY4YWYxZjcyNzRmNDRhN2YzYzMwZWYzOGVhM2QwMzcxMzk0ZDEwNTYyZDA4MThjZDE1IiwidGFnIjoiIn0%3D',
+      'Priority' => 'u=0, i',
+      'Referer' => 'https://www.google.com/',
+      'Sec-Ch-Ua' => '"Opera";v="111", "Chromium";v="125", "Not.A/Brand";v="24"',
+      'Sec-Ch-Ua-Mobile' => '?0',
+      'Sec-Ch-Ua-Platform' => '"Windows"',
+      'Sec-Fetch-Dest' => 'document',
+      'Sec-Fetch-Mode' => 'navigate',
+      'Sec-Fetch-Site' => 'cross-site',
+      'Upgrade-Insecure-Requests' => '1',
+      'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0'
+    }
 }
